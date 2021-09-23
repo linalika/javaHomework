85b68cadebf54a90f5c311821455c1846f6af409
alina stepanova <gklina9@yandex.ru>
2021-09-23 21:07:13 +0300

    homework7

---
+++ src/main/java/homework7/Main.java
@@ -0,0 +1,28 @@
+package homework7;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("295212")// <- 8873 code passthru parameter on method
+                .addQueryParameter("apikey", "Rc7c0nAiSJsey6vtNf1fi4eQvlXlzAXE")
+                .addQueryParameter("language", "en")
+                .build();
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        String responseBody = response.body().string();
+        System.out.println(responseBody);
+    }
+}
